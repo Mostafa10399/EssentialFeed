a9e8b2f6420a6500e8515af02a0bea75f758593d
MostafaMahmoud972 <mostafa.mahmoud@mtor.co>
2024-08-03 19:42:03 +0300

    combine spy captured into a single type(tuble)

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -67,17 +67,18 @@ final class RemoteFeedLoaderTests: XCTestCase {
     }
     
     private class HTTPClientSpy: HTTPClient {
-        var requestedURLs = [URL]()
-        var completions = [(Error) -> Void]()
+        var requestedURLs: [URL] {
+            messages.map { $0.url }
+        }
+        private var messages = [(url: URL, completion: (Error) -> Void)]()
         func getUrl(
             url: URL,
             completion: @escaping (Error) -> Void) {
-                requestedURLs.append(url)
-                completions.append(completion)
+                messages.append((url, completion))
             }
         
         func complete(with error: Error, index: Int = 0) {
-            completions[index](error)
+            messages[index].completion(error)
         }
     }
 }
